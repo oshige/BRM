f9eaafaf5898e6848bbfe1d6cd00708274834051
Mitsuru Nagashima <nshima326@gmail.com>
2013-04-14 23:15:15 +0900

    calcNumbers

--- src/hello.rb
+++ src/hello.rb
@@ -2,4 +2,45 @@ class TodaysConversation
 	def hello
 		"おなかいっぱい"
 	end
+
+	def howOldAreU(name)
+		case name
+		when "Sayoko"
+			return 28
+		when "Mitsuru"
+			return 27
+		else 
+			return 0
+		end
+	end
+
+	def addNumbers(firstNum, secondNum)
+		return firstNum + secondNum
+	end
+
+	def substractNumbers(firstNum, secondNum)
+		return firstNum - secondNum
+	end
+
+	def devideNumbers(firstNum, secondNum)
+		return firstNum / secondNum
+	end
+
+	def multipleNumbers(firstNum, secondNum)	
+		return firstNum * secondNum
+	end
+
+	def calcNumbers(mode, firstNum, secondNum)
+		case mode
+		when "add"
+			addNumbers(firstNum, secondNum)
+		when "substract"
+			substractNumbers(firstNum, secondNum)
+		when "multiple"
+			multipleNumbers(firstNum, secondNum)
+		when "devide"
+			devideNumbers(firstNum, secondNum)
+		end		
+	end
+
 end
--- test/test_hello.rb
+++ test/test_hello.rb
@@ -14,4 +14,22 @@ class TC_Hello1 < Test::Unit::TestCase
 	def test_hello_right
 		assert_equal("おなかいっぱい", @obj.hello, "正しいメッセージ")
 	end
+
+	def test_howOldAreU
+		assert_equal(28, @obj.howOldAreU("Sayoko"))
+		assert_equal(27, @obj.howOldAreU("Mitsuru"))
+		assert_equal(0, @obj.howOldAreU("Somebody"))
+	end
+
+	def test_addNumbers
+		assert_equal(25, @obj.addNumbers(15, 10))
+	end
+
+	def test_calcNumbers
+		assert_equal(30, @obj.calcNumbers("add", 27, 3))
+		assert_equal(40, @obj.calcNumbers("substract", 45, 5))
+		assert_equal(45, @obj.calcNumbers("multiple", 15, 3))
+		assert_equal(50, @obj.calcNumbers("devide", 150, 3))
+	end	
+
 end
